b2e6efdca46b4761c346cf178a6384eea6fdbdcc
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-05-15 11:43:29 -0700

    set up user context

--- src/contexts/auth0-context.js
+++ src/contexts/auth0-context.js
@@ -1,17 +1,46 @@
 import React, { Component, createContext } from "react";
+import createAuth0Client from "@auth0/auth0-spa-js";
 
 // create the context
 export const Auth0Context = createContext();
 
 // create a provider
 export class Auth0Provider extends Component {
-  state = { message: "testing message here!" };
+  // sets initial auth0 client state to null
+  state = {
+    auth0Client: null,
+    isLoading: true,
+    isAuthenticated: false,
+    user: null,
+  };
+
+  // sets config info from my .env file
+  config = {
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    redirect_uri: window.location.origin,
+  };
+
+  // When component loads, initialize Auth0 client
+  componentDidMount() {
+    this.initializeAuth0();
+  }
+
+  // initialize the auth0 library
+  initializeAuth0 = async () => {
+    const auth0Client = await createAuth0Client(this.config);
+    const isAuthenticated = await auth0Client.isAuthenticated();
+    const user = isAuthenticated ? await auth0Client.getUser() : null;
+    this.setState({ auth0Client, isLoading: false, isAuthenticated, user });
+  };
+
+  // making pizza
 
   render() {
-    const { message } = this.state;
+    const { isLoading, isAuthenticated, user } = this.state;
     const { children } = this.props;
 
-    const configObject = { message };
+    const configObject = { isLoading, isAuthenticated, user };
 
     return (
       <Auth0Context.Provider value={configObject}>
